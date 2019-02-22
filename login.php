85daa7c816ff775be7044eadd23363c5723e5fa5
Nick Skalenko <nikolasthebest839@gmail.com>
2019-02-23 00:17:35 +0300

    Modified forms and style in files: login.php, registration.php

--- login.php
+++ login.php
@@ -37,20 +37,35 @@ if(password_verify($_POST['password'], $row_pass[0])){
 }
 ?>
 
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" href="bootstrap/css/bootstrap.min.css">
+	<style>
+		body {
+			background-image: url("./enter.jpg");
+		}
+	</style>
+	</head>
+<body>
+<div class="jumbotron container" align="center">
 <form action="./login.php" method="POST">
-
-<p>
-	<p><strong>Логин</strong></p>
-	<p><input type="text" name="login"></p>
-</p>
-
-<p>
-	<p><strong>Пароль</strong></p>
-	<input type="password" name="password">
-</p>
-
-<p>
-	<button type="submit" name="do_login">Войти</button>
-</p>
-
+  <div class="form-group">
+    <label for="exampleInputEmail1"><strong>Логин</strong></label>
+    <input type="text" class="form-control form-control" name="login" placeholder="Введите логин">
+    <small id="emailHelp" class="form-text text-muted">Мы никогда не рассказываем пароли пользователей!</small>
+  </div>
+  <div class="form-group">
+  <label for="exampleInputPassword1"><strong>Пароль</strong></label>
+    <input type="password" class="form-control" name="password" placeholder="Введите пароль">
+  </div>
+  <button type="submit" name="do_login" class="btn btn-primary">Войти</button>
 </form>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- registration.php
+++ registration.php
@@ -2,12 +2,35 @@
 require "dtabase/db.php";
 ?>
 
-<?php if(isset($_SESSION['remb_user'])){ ?>
-	Авторизован!<br>
-	<hr>
-	<a href="./logout.php">Выйти</a>
-<?php }else{ ?>
-<div style="color: red;">Вы не авторизованы!</div><hr>
-<a href="./login.php">Авторизация</a><br>
-<a href="./signup.php">Регистрация</a>
-<?php }; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" href="bootstrap/css/bootstrap.min.css">
+	<style>
+		body {
+			background-image: url("./vegetables.jpg");
+		}
+	</style>
+	</head>
+<body>
+<div class="jumbotron container" align="center">
+	<?php if(isset($_SESSION['remb_user'])){ ?>
+	<h1 class="display-4">Вы авторизованы!</h1>
+	<a class="btn btn-primary btn-lg" href="./logout.php"" role="button">Выход</a>
+	<?php }else{ ?>
+  <h1 class="display-4">Вы не авторизованы!</h1>
+  <p class="lead">Мы рады приветствовать вас на нашем сайте!</p>
+  <hr class="my-4">
+  <p>Для дальнейшей работы на сайте просьба выбрать действие:</p>
+  <a class="btn btn-primary btn-lg" href="./login.php"" role="button">Авторизация</a>
+  <a class="btn btn-primary btn-lg" href="./signup.php"" role="button">Регистрация</a>
+  <?php }; ?>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
